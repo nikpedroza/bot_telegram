dd6a961f02cfd633836dea794a655e8ac0445ac8
Nicolas <nikitopedroza@gmail.com>
2025-08-14 00:02:13 -0300

    Cambio de nombre

--- datos_users.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-#Guardamos el el Nombre del usuario 
-def guardar_user(id,name,text):
-    fecha = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    with open("users.txt", "a", ) as f:
-        f.writelines(f"[{fecha}] ({id}){name} : {text}")
-        f.write("\n")
