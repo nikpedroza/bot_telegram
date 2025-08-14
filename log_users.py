1a7831cc95f678e19e861f0021e65a9408831324
Nicolas <nikitopedroza@gmail.com>
2025-08-14 00:12:39 -0300

    UTF-8

--- log_users.py
+++ log_users.py
@@ -3,6 +3,6 @@ import datetime
 #Guardamos el Nombre del usuario 
 def guardar_user(id,name,text):
     fecha = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    with open("log.txt", "a", ) as f:
+    with open("log.txt", "a",encoding="UTF-8") as f:
         f.writelines(f"[{fecha}] ({id}){name} : {text}")
         f.write("\n")
