e78cdcb82bd63bbd091811e1095bb657b41afd63
Nicolas <nikitopedroza@gmail.com>
2025-08-16 03:20:56 -0300

    Agregamos ubicacion de ser necesario

--- log_users.py
+++ log_users.py
@@ -1,8 +1,12 @@
 import datetime
 
 #Guardamos el Nombre del usuario 
-def guardar_user(id,name,text):
+def guardar_user(id,name,text,lat=None,lon=None):
     fecha = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     with open("log.txt", "a",encoding="UTF-8") as f:
-        f.writelines(f"[{fecha}] ({id}){name} : {text}")
-        f.write("\n")
+        if lat is not None or lon is not None:
+            f.writelines(f"[{fecha}] ({id}){name} : {text} LAT:{lat} LON:{lon}")
+            f.write("\n")
+        else:
+            f.writelines(f"[{fecha}] ({id}){name} : {text}")
+            f.write("\n")
